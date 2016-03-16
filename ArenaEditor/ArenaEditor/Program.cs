9420a2667e05aa8224866a407d4853fb892653d4
Udsahn <udsahn@gmail.com>
2016-03-16 17:46:03 -0400

    Initial Commit

---
+++ ArenaEditor/ArenaEditor/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArenaEditor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The code to this editor is still being implimented.");
+            Console.WriteLine();
+            Console.WriteLine("Check the 'Editor' branch for the absolute latest updates.");
+            Console.ReadKey();
+        }
+    }
+}
